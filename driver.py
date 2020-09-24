5975020068e1b7d32233285c290c3e7b5c372235
Oded Wertheimer <oded.wertheimer@mail.huji.ac.il>
2020-09-24 14:36:48 +0300

    changing something in the driver.py

--- driver.py
+++ driver.py
@@ -3,20 +3,20 @@ from time import time
 import scipy.linalg as linalg
 
 # %%
-network = Network(1024, 4, 2, -1.2, 0.1, 9000e-1, np.tanh, 50, 100, 5, 2e5, 0.1118)
+network = Network(512, 4, 2, -1.2, 0.1, 9000e-1, np.tanh, 50, 100, 5, 2e5, 0.1118)
 # %%
 first_W = network.W.copy()
 
 start = time()
 network.run_first_phase()
-overall = time() - start
+overall = (time() - start)/60.
 # %%
 network.W - first_W
 # %%
 W = network.W
 P = network.P
 # %%
-c1 = ((network.P[0, 0, :] @ network.W[:, 0]) / network.P[0, 0, :])
+c1 = ((network.P[0, 0, :] @ network.W[:, 0]) / network.P[0, 0, 0])
 
 # %%
 t = 2
